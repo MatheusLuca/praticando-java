a8926887eee169b6ff07b3552668683609e97fd9
matheusluca <luca_matheus@live.com>
2026-04-28 20:43:09 -0300

    Feat: primeiro commit

---
+++ praticando-java/src/br/com/alura/praticando/java/model/Produto.java
@@ -0,0 +1,41 @@
+package br.com.alura.praticando.java.model;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Produto: %s
+                Preco: %s
+                Quantidade:%.2f 
+                """.formatted(this.nome, this.preco, this.quantidade);
+    }
+}
